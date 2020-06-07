e1690d14259c375424a7fdf0dbdfda9161e0d2ff
Marcel Goh <marcel.goh@mail.mcgill.ca>
2020-06-07 15:36:17 -0400

    Refactored PostScript settings

--- src/main.ml
+++ src/main.ml
@@ -10,8 +10,12 @@ let main () =
   let start_time = Sys.time () in
   let grid = N.fill_para (30.0, -10.0) (5.0, 10.0) 1.0 (10.0, 5.0) in
   let fill_time = Sys.time () -. start_time in
+  let fp = P.create_ps_file "out/grid" in
   Printf.printf "Grid built in: %fs\n" (Sys.time() -. start_time);
-  P.plot_grid grid 7.0 7.0 P.red P.blue "out/grid";
+  P.xtick fp P.default (-30);
+  P.xtick fp P.default 30;
+  P.plot_grid fp P.default grid;
+  P.close_ps_file fp;
   let total_time = Sys.time () -. start_time in
   Printf.printf "Grid plotted in: %fs\n" (total_time -. fill_time);
   Printf.printf "%d points plotted.\n" (N.grid_size grid)
--- src/postscript.ml
+++ src/postscript.ml
@@ -11,21 +11,56 @@ let blue = (0.0, 0.0, 1.0)
 let black = (0.0, 0.0, 0.0)
 let purple = (0.5, 0.0, 1.0)
 
+type settings = {
+  xscale : float;
+  yscale : float;
+  colour1 : float * float * float;
+  colour2 : float * float * float;
+  xorigin : int;
+  yorigin : int;
+}
+
+let default = {
+  xscale = 5.0;
+  yscale = 5.0;
+  colour1 = red;
+  colour2 = blue;
+  xorigin = 306;
+  yorigin = 396;
+}
+
 let file = "/out/plot"
-let preamble = "%!PS\n/dot { 1.5 0 360 arc closepath fill } def\n"
+let preamble ="
+%!PS
+/dot { 1.5 0 360 arc closepath fill } def
+/Courier findfont 9 scalefont setfont
+/ytick { newpath 0 exch moveto -5 0 rmoveto 10 0 rlineto stroke} def
+/xtick { newpath 0 moveto 0 -5 rmoveto 0 10 rlineto stroke} def
+"
+
+let xtick fp stgs (tick : int) =
+  let x = float_of_int tick *. stgs.xscale in
+  output_string fp (sprintf "%f xtick\n" x);
+  output_string fp (sprintf "%f -7 moveto (%d) show\n" x tick)
 
 (* create_ps_file filename creates (overwrites) <filename>.ps *)
-let create_ps_file filename = open_out (filename ^ ".ps")
-
-let plot_grid (grid : N.grid) xscale yscale colour1 colour2 filename =
-  let (r1, g1, b1) = colour1 in
-  let (r2, g2, b2) = colour2 in
-  let fp = create_ps_file filename in
-  let num_pts = float_of_int (N.grid_size grid) in
+let create_ps_file filename =
+  let fp = open_out (filename ^ ".ps") in
   output_string fp preamble;
-  output_string fp "306 396 translate\n0.5 setlinewidth\n";  (* put origin at centre *)
-  output_string fp "newpath 0 -396 moveto 0 396 lineto stroke\n"; (* x-axis *)
-  output_string fp "newpath -306 0 moveto 306 0 lineto stroke\n"; (* y-axis *)
+  fp
+
+let close_ps_file fp =
+  output_string fp "showpage\n";
+  close_out fp
+
+let plot_grid fp stgs grid =
+  let (r1, g1, b1) = stgs.colour1 in
+  let (r2, g2, b2) = stgs.colour2 in
+  let (xo, yo) = (stgs.xorigin, stgs.yorigin) in
+  let num_pts = float_of_int (N.grid_size grid) in
+  output_string fp (sprintf "%d %d translate\n0.5 setlinewidth\n" xo yo);
+  output_string fp (sprintf "newpath 0 -%d moveto 0 %d lineto stroke\n" yo (yo + 792));
+  output_string fp (sprintf "newpath -%d 0 moveto %d 0 lineto stroke\n" xo (xo + 612));
   let set_colour (r, g, b) = output_string fp (sprintf "%f %f %f setrgbcolor\n" r g b) in
   let print_point idx (x, y) =
     let t = (float_of_int idx) /. num_pts in
@@ -33,8 +68,7 @@ let plot_grid (grid : N.grid) xscale yscale colour1 colour2 filename =
     let g = g1 +. (g2 -. g1) *. t in
     let b = b1 +. (b2 -. b1) *. t in
     set_colour (r, g, b);
-    output_string fp (sprintf "%f %f dot\n" (x *. xscale) (y *. yscale))
+    output_string fp (sprintf "%f %f dot\n" (x *. stgs.xscale) (y *. stgs.yscale))
   in
   D.iteri print_point grid;  (* print every point in the grid *)
-  output_string fp "showpage\n";
-  close_out fp;
+  set_colour black
